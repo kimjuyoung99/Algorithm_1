80a973674ce574bfca99014df77bb7e67c79a6a3
kimjuyoung99 <jjassb404@naver.com>
2024-03-17 15:45:38 +0900

    배열뒤집기

---
+++ 배열뒤집기.js
@@ -0,0 +1,13 @@
+// 문제 설명
+// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. 
+// num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+// _______________________________
+
+function solution(num_list) {
+    var answer = [];
+
+    return answer = num_list.reverse();
+}
+
+// 매우 쉬움. 하지만! reverse()함수가 있다는 사실은 검색해서 앎^^
